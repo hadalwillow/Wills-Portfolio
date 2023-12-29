df02e66e75b66dba2905ba3642d6130a219b9840
Will Phillips <80987836+willadelinephillips@users.noreply.github.com>
2023-12-29 14:46:30 -0800

    Added CSS to Projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Graph1 from '../media/razorcostanalysis.png';
+import '../css/Projects.css';
 
 function Projects() {
     return (
         <div>
         <h1>Environmental Project Analysis #1: Plastic-Free Razors</h1>
-        <img src= {Graph1} alt="Graph showing cost analysis between plastic and straight razors" width="1000" height="600"/>
+        <p>My personal experience switching from Flamingo razors to an all-metal razor that takes straight blades instead of plastic heads. Factored into starting cost was the cost
+            of both razors. It is important, as with all personal projects, to keep in mind that these results are very specific to me. I use a razor head for a total of one month 
+            before I switch it out with another one, for example. I am also very clumsy, and cannot use a more simple, and therefor cheaper, straight razor. 
+            <img src= {Graph1} alt="Graph showing cost analysis between plastic and straight razors" width="80%" height="80%"/>
+        </p>
         </div>
     );
 }
