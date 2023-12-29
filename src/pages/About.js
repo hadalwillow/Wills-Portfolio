706dccb5bad2c88f25e7f4e530921478285330df
Will Phillips <80987836+willadelinephillips@users.noreply.github.com>
2023-12-29 14:27:40 -0800

    Added graph image
    Added analysis graph into Projects page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function About() {
     return (
+        <div>
         <h1>This is also not supremely impressive, I admit. But there is more incoming, I promise.</h1>
+        </div>
     );
 }
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Graph1 from '../media/razorcostanalysis.png';
 
 function Projects() {
     return (
+        <div>
         <h1>Environmental Project Analysis #1: Plastic-Free Razors</h1>
+        <img src= {Graph1} alt="Graph showing cost analysis between plastic and straight razors" width="1000" height="600"/>
+        </div>
     );
 }
 
