1bd046786e77a87d01193d4f4f7d4ae65e092f56
Will Phillips <80987836+willadelinephillips@users.noreply.github.com>
2024-01-08 22:44:22 -0800

    Math and text size

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,24 +7,24 @@ function Home() {
         <h1>My name is Willow Phillips.  I'm a creative with a passing interest in everything all the time. Welcome, please dive in.</h1>
         <div id="epipelagic">
             <h2>Epipelagic Zone - The Sunlight Zone</h2>
-            <p> Surface level to 200 meters in depth.</p>
+            <h3> Surface level to 200 meters below.</h3>
         </div>
         <div id="mesopelagic">
             <h2>Mesopelagic Zone - The Twilight Zone</h2>
-            <p>200 to 1000 meters.</p>
+            <h3>200 to 1000 meters below sea level.</h3>
         </div>
         <div id="bathypelagic">
             <h2>Bathypelagic Zone - The Midnight Zone</h2>
-            <p>1000 to 4000 meters.</p>
+            <h3>1000 to 4000 meters below sea level.</h3>
         </div>
         <div id="abyssopelagic">
             <h2>Abyssopelagic Zone - The Abyssal Zone</h2>
-            <p>4000 to 6000 meters.</p>
+            <h3>4000 to 6000 meters below sea level.</h3>
         </div>
         <div id="hadalpelagic">
             <h2>Hadalpelagic Zone - The Hadal Zone</h2>
-            <h3>(also known as - The Trenches)</h3>
-            <p>4000 to 6000 meters.</p>
+            <p>(also known as - The Trenches)</p>
+            <h3>6000 to 11000 meters below sea level.</h3>
         </div>
         </div>
     );
