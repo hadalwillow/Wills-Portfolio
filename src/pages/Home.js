61c8c18e79413b8cae2e866e17b0bc6095eff75f
Will Phillips <80987836+willadelinephillips@users.noreply.github.com>
2024-01-08 23:20:28 -0800

    zone classes added, audio centered

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,26 +7,26 @@ function Home() {
         <div id="home">
         <h1>My name is Willow Phillips.  I'm a creative with a passing interest in everything all the time. Welcome, please dive in.</h1>
         <h2>(Optional Ambience)</h2>
-        <audio controls autoplay loop src={underwater} type="audio/mp3">
+        <div id="homeaudioplayer"><audio controls autoplay loop src={underwater} type="audio/mp3">
                 Error: your web browser does not support this audio player.
-            </audio> 
-        <div id="epipelagic">
+            </audio></div>
+        <div class="zone" id="epipelagic">
             <h2>Epipelagic Zone - The Sunlight Zone</h2>
             <h3> Surface level to 200 meters below.</h3>
         </div>
-        <div id="mesopelagic">
+        <div class="zone" id="mesopelagic">
             <h2>Mesopelagic Zone - The Twilight Zone</h2>
             <h3>200 to 1000 meters below sea level.</h3>
         </div>
-        <div id="bathypelagic">
+        <div class="zone" id="bathypelagic">
             <h2>Bathypelagic Zone - The Midnight Zone</h2>
             <h3>1000 to 4000 meters below sea level.</h3>
         </div>
-        <div id="abyssopelagic">
+        <div class="zone" id="abyssopelagic">
             <h2>Abyssopelagic Zone - The Abyssal Zone</h2>
             <h3>4000 to 6000 meters below sea level.</h3>
         </div>
-        <div id="hadalpelagic">
+        <div class="zone" id="hadalpelagic">
             <h2>Hadalpelagic Zone - The Hadal Zone</h2>
             <p>(also known as - The Trenches)</p>
             <h3>6000 to 11000 meters below sea level.</h3>
