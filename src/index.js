d233f8d8419deecb3d0787a49d01cac784b7305f
Will Phillips <80987836+willadelinephillips@users.noreply.github.com>
2024-02-25 13:17:54 -0800

    Dialog navbar gapping bug

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Archive from "./pages/Archive";
+import Dialog from "./pages/Demos/Dialog"
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
           <Route path="archive" element={<Archive />} />
+          <Route path="dialog" element={<Dialog />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Demos/Dialog.js
+++ src/pages/Demos/Dialog.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import Graph1 from '../media/razorcostanalysis.png';
-import '../css/Projects.css';
-import { Link } from "react-router-dom";
 
-function Projects() {
+
+function Dialog() {
     return (
-        <div id="projects">
+        <div id="dialog">
             <h1>Projects</h1>
             <h2>Current Projects</h2>
             <h3> Portfolio Website Buglist</h3>
             <ul>
                 <li>The radio won't turn off on the main page, something to do with react useState</li>
-                <li>Dialog has navbar gapping issue</li>
             </ul>
-            <h3><Link to="/dialog">Dialog box simulator</Link></h3>
             
             <h2>Personal Project Archive</h2>
             <h3>Plastic-Free Razors</h3>
                 <p>My personal experience switching from Flamingo razors to an all-metal razor that takes straight blades instead of plastic heads. Factored into starting cost was the cost
                     of both razors. It is important, as with all personal projects, to keep in mind that these results are very specific to me. I use a razor head for a total of one month 
                     before I switch it out with another one, for example. I am also very clumsy, and cannot use a more simple, and therefor cheaper, straight razor. 
-                    <img src= {Graph1} alt="Graph showing cost analysis between plastic and straight razors" width="80%" height="80%"/>
                 </p>
         </div>
     );
 }
 
-export default Projects;
+export default Dialog;
